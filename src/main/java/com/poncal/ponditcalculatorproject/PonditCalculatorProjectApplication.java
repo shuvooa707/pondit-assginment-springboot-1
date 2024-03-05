03d3d4e22362f257e267d24ba7c8ec13e263f5b5
Shuvo Sarker <shuvooa707@gmail.com>
2024-03-06 03:59:24 +0600

    Home Page added

--- src/main/java/com/poncal/ponditcalculatorproject/PonditCalculatorProjectApplication.java
+++ src/main/java/com/poncal/ponditcalculatorproject/PonditCalculatorProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PonditCalculatorProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PonditCalculatorProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PonditCalculatorProjectApplication.class, args);
+    }
 
 }
--- src/main/java/com/poncal/ponditcalculatorproject/controller/HomeController.java
+++ src/main/java/com/poncal/ponditcalculatorproject/controller/HomeController.java
@@ -1,4 +1,51 @@
 package com.poncal.ponditcalculatorproject.controller;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
 public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Calculator";
+    }
+    @PostMapping("/add")
+    public ResponseEntity<?> add(@RequestParam(name = "num1") Double num1, @RequestParam(name = "num2") Double num2) {
+        Response response = new Response(String.valueOf(num1), String.valueOf(num2), "Addition", String.valueOf(num1+num2));
+        return new ResponseEntity<>(response, OK);
+    }
+    @PostMapping("/sub")
+    public ResponseEntity<?> sub(@RequestParam(name = "num1") Double num1, @RequestParam(name = "num2") Double num2) {
+        Response response = new Response(String.valueOf(num1), String.valueOf(num2), "Subtraction", String.valueOf(num1-num2));
+        return new ResponseEntity<>(response, OK);
+    }
+    @PostMapping("/mul")
+    public ResponseEntity<?> mul(@RequestParam(name = "num1") Double num1, @RequestParam(name = "num2") Double num2) {
+        Response response = new Response(String.valueOf(num1), String.valueOf(num2), "Multiplication", String.valueOf(num1*num2));
+        return new ResponseEntity<>(response, OK);
+    }
+    @PostMapping("/div")
+    public ResponseEntity<?> div(@RequestParam(name = "num1") Double num1, @RequestParam(name = "num2") Double num2) {
+        Response response = new Response(String.valueOf(num1), String.valueOf(num2), "Division", String.valueOf(num1/num2));
+        return new ResponseEntity<>(response, OK);
+    }
+}
+
+
+@Data
+@AllArgsConstructor
+class Response {
+    public String number1;
+    public String number2;
+    public String operation;
+    public String result;
 }
